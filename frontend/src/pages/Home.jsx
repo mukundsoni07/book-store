3e273126970ebef3cdc52f339be915881f2d9fe8
Mukund Soni <59203417+mukundsoni07@users.noreply.github.com>
2024-10-17 15:31:30 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://book-store-flame-phi.vercel.app/?vercelToolbarCode=QN5sYGvPPwkSHx2')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
